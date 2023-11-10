7982a1f19c132a2ef522060ccf7105fb33c07ff2
AstvatEreta-Lens <ashabuljannah201@gmail.com>
2023-11-10 23:33:06 +0700

    add THT6 v.1

---
+++ THT6/assets/script/script.js
@@ -0,0 +1,26 @@
+
+const toggleButton = document.querySelector(".toggle-button");
+const menu = document.querySelector(".menu");
+
+toggleButton.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+
+
+document.getElementById('myForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Menghentikan pengajuan formulir standar
+
+  // Mendapatkan nilai dari setiap input
+  var nameValue = document.getElementsByName('name')[0].value;
+  var emailValue = document.getElementsByName('email')[0].value;
+  var satisfactionValue = document.getElementsByName('satisfaction')[0].value;
+  var feedbackValue = document.getElementsByName('feedback')[0].value;
+
+  // Menampilkan nilai pada elemen output
+  document.getElementById('output').innerHTML = `
+      <p>Name: ${nameValue}</p>
+      <p>Email: ${emailValue}</p>
+      <p>Satisfaction: ${satisfactionValue}</p>
+      <p>Feedback: ${feedbackValue}</p>
+  `;
+});
